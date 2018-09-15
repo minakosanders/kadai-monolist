8065aef18dd4a37730d67a807c76debd921e1aaf
minako.sanders <fb030323@yahoo.co.jp>
2018-09-15 21:35:05 +0000

    ranking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @items = @user.items.uniq
+    @count_want = @user.want_items.count
+    @count_have = @user.have_items.count
   end
 
   def new
-    @user = User.find(params[:id])
-    @items = @user.items.uniq
-    @count_want = @user.want_items.count
+    @user = User.new
   end
 
   def create
